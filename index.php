abe4278ae04246e27fa45e302d4135ac471177df
Sagendar\Jethif <domoker@hotmail.com>
2017-11-03 09:12:28 +0100

    Added First Layout

--- index.php
+++ index.php
@@ -1,7 +1,150 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jethif
- * Date: 06/10/2017
- * Time: 08:32
- */
+<!DOCTYPE HTML>
+<!--
+	Striped by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>LibraryOfKnowledge</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Content -->
+			<div id="content">
+				<div class="inner">
+
+					<!-- Post -->
+						<article class="box post post-excerpt">
+							<header>
+								<!--
+									Note: Titles and subtitles will wrap automatically when necessary, so don't worry
+									if they get too long. You can also remove the <p> entirely if you don't
+									need a subtitle.
+								-->
+								<h2><a href="#">Vagrant installation and usage</a></h2>
+								<p>In this article we will explain, what exactly is vagrant, how you install it and how you can use it</p>
+							</header>
+							<div class="info">
+								<!--
+									Note: The date should be formatted exactly as it's shown below. In particular, the
+									"least significant" characters of the month should be encapsulated in a <span>
+									element to denote what gets dropped in 1200px mode (eg. the "uary" in "January").
+									Oh, and if you don't need a date for a particular page or post you can simply delete
+									the entire "date" element.
+
+								-->
+								<span class="date"><span class="month">Jul<span>y</span></span> <span class="day">14</span><span class="year">, 2014</span></span>
+								<!--
+									Note: You can change the number of list items in "stats" to whatever you want.
+								-->
+								<ul class="stats">
+									<li><a href="#" >Dominik</a></li>
+								</ul>
+							</div>
+							<a href="#" class="image featured"><img src="images/vagrant.png" alt="" /></a>
+							<p>
+								Vagrant is a tool for building and managing virtual machine environments in a single workflow. With an easy-to-use workflow and focus on automation, Vagrant lowers development environment setup time, increases production parity, and makes the "works on my machine" excuse a relic of the past.
+
+								If you are already familiar with the basics of Vagrant, the documentation provides a better reference build for all available features and internals.
+							</p>
+						</article>
+
+					<!-- Post -->
+						<article class="box post post-excerpt">
+							<header>
+								<h2><a href="#">PHP</a></h2>
+								<p>What are the advantages and disadvantages of using PHP?</p>
+							</header>
+							<div class="info">
+								<span class="date"><span class="month">Jul<span>y</span></span> <span class="day">8</span><span class="year">, 2014</span></span>
+								<ul class="stats">
+									<li><a href="#">David</a></li>
+								</ul>
+							</div>
+							<a href="#" class="image featured"><img src="images/php.png" alt="" /></a>
+							<p>
+								Advantages of PHP
+
+								Open source: It is developed and maintained by a large group of PHP developers, this will helps in creating a support community, abundant extension library.
+								Speed: It is relative fast since it uses much system resource.
+								Easy to use: It uses C like syntax, so for those who are familiar with C, it’s very easy for them to pick up and it is very easy to create website scripts.
+								Stable: Since it is maintained by many developers, so when bugs are found, it can be quickly fixed.
+								Powerful library support: You can easily find functional modules you need such ...
+							</p>
+						</article>
+
+					<!-- Pagination -->
+						<div class="pagination">
+							<!--<a href="#" class="button previous">Previous Page</a>-->
+							<div class="pages">
+								<a href="#" class="active">1</a>
+								<a href="#">2</a>
+								<a href="#">3</a>
+								<a href="#">4</a>
+								<span>&hellip;</span>
+								<a href="#">20</a>
+							</div>
+							<a href="#" class="button next">Next Page</a>
+						</div>
+
+				</div>
+			</div>
+
+		<!-- Sidebar -->
+			<div id="sidebar">
+
+				<!-- Logo -->
+					<h1 id="logo"><a href="#">LibraryOfKnowledge</a></h1>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li class="current"><a href="#">Documentations</a></li>
+							<li><a href="#">Questions</a></li>
+							<li><a href="#">New Post</a></li>
+							<li><a href="#">Profil</a></li>
+						</ul>
+					</nav>
+
+				<!-- Search -->
+					<section class="box search">
+						<form method="post" action="#">
+							<input type="text" class="text" name="search" placeholder="Search" />
+						</form>
+					</section>
+
+				<!-- Recent Posts -->
+					<section class="box recent-posts">
+						<header>
+							<h2>Recent Posts</h2>
+						</header>
+						<ul>
+							<li><a href="#">Vagrant installation and usage</a></li>
+							<li><a href="#">PHP</a></li>
+							<li><a href="#">Ajax command not responding</a></li>
+							<li><a href="#">Can't access website on local apache server</a></li>
+						</ul>
+					</section>
+
+				<!-- Copyright -->
+					<ul id="copyright">
+						<li>&copy; LibraryOfKnowledge.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
